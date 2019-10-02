a93f9c01ee21d89c88567602723adf15ad01f2ff
sonseungwoo <skyghy3342@naver.com>
2019-10-02 21:35:36 +0900

    Initial commit

---
+++ src/main/java/me/seungwoo/springrestdocsexample/SpringRestDocsExampleApplication.java
@@ -0,0 +1,13 @@
+package me.seungwoo.springrestdocsexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestDocsExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestDocsExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/seungwoo/springrestdocsexample/SpringRestDocsExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package me.seungwoo.springrestdocsexample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringRestDocsExampleApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
